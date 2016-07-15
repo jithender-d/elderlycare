903b210e303212692ecf43dc94cdd203b84378ad
jithender-d <jithender.dunnala@gmail.com>
2016-07-15 12:42:26 +0530

    allpy the service calls

--- home/service.js
+++ home/service.js
@@ -3,56 +3,24 @@ service.js
 This service deals with the get post mothods for ajax call
 */
 myApp.factory('ajaxwebservice', function($http,$location) {
-    //base url formation logic start
-    var baseUrl = $location.protocol()+"://"+$location.host();
-    if($location.port() != ""){
-        baseUrl = baseUrl +":"+$location.port();
-    }
-    baseUrl = baseUrl +"/";
-    //base url formation logic end
-
     var factory = {}; 
     
-    /**
-    * getPost
-    *
-    *@description :: This service deals with the get & post methods
-    * 
-    * inputs:
-    * @ajaxmethod - get/post
-    * @data - data to send for request
-    * @url - restapi url
-    *
-    * output:
-    * it will give promise object of getpost service 
-    */
-    factory.getPost = function(ajaxmethod,data,url) {
-        /*return $http({
-            url: url, 
-            method: ajaxmethod,
-            data: 'grant_type=password&client_id=3MVG9ZL0ppGP5UrC3MHbP86G8oH_wQr7bZdNFleh3UC9ZM1SwtC4hTUrJ.YRywLf4_rfw6nLJYdqEYlJYJ18i&client_secret=2678223884336568441&username=ElderlyCare@Popcornapps.com&password=pop@12345duYlrjYafzhodOX6aWppN7qZU',
-            headers: {
-                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Cache-Control': 'no-cache'
-            }
-        });*/
-
+    factory.getPost = function(ajaxmethod,token,url, data) {
         return $http({
-            url: url,
-            method: ajaxmethod,
-            data: data,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Cache-Control': 'no-cache',
-            }
+            url: 'http://localhost/accessToken.php?flag=2&url='+url+'&postd='+data+'&met=POST&token='+token,
         })
         .then(function(response) {
             return response;
         });
+    };
 
-
-
-        
+    factory.getAccessToken = function() {
+        return $http({
+            url: 'http://localhost/accessToken.php?flag=1',
+        })
+        .then(function(response) {
+            return response;
+        });
     };
     return factory;
 });
