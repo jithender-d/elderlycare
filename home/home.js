bc8aaaf686706eab7c12cf5fb9ef8a6d6b69b6e3
jithender-d <jithender.dunnala@gmail.com>
2016-07-15 12:43:53 +0530

    allpy the service calls

--- home/home.js
+++ home/home.js
@@ -1,12 +1,14 @@
 'use strict';
 // configure your hostname here
 myApp.controller('HomeController', function($scope, ajaxwebservice) {
-	// $scope.regUser = '1';
-	var url = 'https://ap2.salesforce.com/services/oauth2/token';
-	var data = 'grant_type=password&client_id=3MVG9ZL0ppGP5UrC3MHbP86G8oH_wQr7bZdNFleh3UC9ZM1SwtC4hTUrJ.YRywLf4_rfw6nLJYdqEYlJYJ18i&client_secret=2678223884336568441&username=ElderlyCare@Popcornapps.com&password=pop@12345duYlrjYafzhodOX6aWppN7qZU';
-	// var res = ajaxwebservice.getPost('POST',data,url);
-	// console.log(res);
-	$scope.accessToken = '00D28000001dZef!ARcAQHHZuO5PCQTt.ziv8mKUjATr6yLBS.HDKtAYUl5tF8UNkPJDGalKKA1Qc.XkFeSahTkWoL0CXjPNtROyw_2WY0IjWAwv';
+
+	$scope.getAccessToken = function() {
+		var res = ajaxwebservice.getAccessToken();
+		if(res !== '') {
+			localStorage.setItem('authInfo', JSON.stringify(res.access_token));
+		}
+	};
+	$scope.getAccessToken();
 });
 
 myApp.controller('RegistrationController', function($scope) {
@@ -59,7 +61,16 @@ myApp.controller('RegistrationController', function($scope) {
 
 myApp.controller('SearchController', function($scope, ajaxwebservice) {
 	var url = 'https://ap2.salesforce.com/services/apexrest/EC_ElderCare';
-	var data = 'authorization:OAuth00D28000001dZef!ARcAQHHZuO5PCQTt.ziv8mKUjATr6yLBS.HDKtAYUl5tF8UNkPJDGalKKA1Qc.XkFeSahTkWoL0CXjPNtROyw_2WY0IjWAwv'
-	var res = ajaxwebservice.getPost('POST',data,url);
-	console.log(res);	
+	
+	$scope.searchHomes = function(zipcode) {
+		var postdata = {};
+		var token = localStorage.getItem('authInfo');
+		console.log(token);
+		postdata.zipcode = $scope.postalCode; 
+		var res = ajaxwebservice.getPost('POST',token,url,postdata);
+
+	};
+
+	$scope.searchHomes();
+	
 });
